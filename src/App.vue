30e03e1c7f70ee0044af4cb58b01a5c236e3a382
Linyer <denglianglin@dgg.net>
2023-02-20 13:45:49 +0800

    feat(deps): 新增prettier和提交相关依赖

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,17 @@
  * @Author: Linyer
  * @Date: 2023-02-17 14:09:09
  * @LastEditors: Linyer
- * @LastEditTime: 2023-02-20 09:07:01
+ * @LastEditTime: 2023-02-20 09:55:52
 -->
 <script setup>
 import HelloWorld from './components/HelloWorld.vue';
 const platform = window.electronAPI.platform;
+
 </script>
 
 <template>
 
-  <HelloWorld msg="111213123123121" />
+  <HelloWorld msg="1112221" />
 </template>
 
 <style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,12 +4,13 @@
  * @Author: Linyer
  * @Date: 2023-02-17 14:09:09
  * @LastEditors: Linyer
- * @LastEditTime: 2023-02-17 17:17:47
+ * @LastEditTime: 2023-02-20 10:26:38
 -->
 <script setup>
-import { ref } from 'vue';
-defineProps({ msg: String });
-const count = ref(0);
+  import { ref } from 'vue'
+
+  defineProps({ msg: String })
+  const count = ref(10)
 </script>
 
 <template>
@@ -17,21 +18,21 @@ const count = ref(0);
 </template>
 
 <style lang="scss" scoped>
-p {
-  a {
-    color: #42b983;
+  p {
+    a {
+      color: #42b983;
+    }
+
+    code {
+      background-color: #eee;
+      padding: 2px 4px;
+      border-radius: 4px;
+      color: #304455;
+    }
   }
 
-  code {
-    background-color: #eee;
-    padding: 2px 4px;
-    border-radius: 4px;
-    color: #304455;
+  label {
+    margin: 0 0.5em;
+    font-weight: bold;
   }
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
 </style>
